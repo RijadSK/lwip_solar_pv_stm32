be350c3d4239aa313b2633fed6f67fb0add85a64
Rijad Skrobo <www.ricky12@live.com>
2024-06-05 18:31:18 +0200

    Added code for Flask web server, together with the short description of the project

---
+++ restapi_good.py
@@ -0,0 +1,34 @@
+import json
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+response_pv = \
+ { 
+   "estimated_actuals": [
+     {
+       "pv_estimate": 0.7698,
+       "period_end": "2024-06-01T09:00:00.0000000Z",
+       "period": "PT30M"
+     },
+     {
+       "pv_estimate": 0.7155,
+       "period_end": "2024-06-01T08:30:00.0000000Z",
+       "period": "PT30M"
+     },
+     {
+       "pv_estimate": 0.6189,
+       "period_end": "2024-06-01T08:00:00.0000000Z",
+       "period": "PT30M"
+     }
+  ]
+}
+
+
+# /rooftop_sites/e5cc-f38e-7730-805d/estimated_actuals?api_key=u70RlALYTh-bWi9lDsuRKxWWi3jQApLz&format=json&hours=1
+@app.route('/rooftop_sites/e5cc-f38e-7730-805d/estimated_actuals', methods=['GET'])
+def get_pv():
+ return jsonify(response_pv)
+
+
+if __name__ == '__main__':
+   app.run(debug=True, host='0.0.0.0', port=10)
